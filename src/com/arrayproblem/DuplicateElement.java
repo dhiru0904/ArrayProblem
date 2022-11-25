3ead45df939d047721a8dd5f6c6aa35a5f74ec57
dhiru0904 <dhirajdevre4@gmail.com>
2022-11-25 23:49:46 +0530

    Added: demonstrate to array of frequency element

--- src/com/arrayproblem/DuplicateElement.java
+++ src/com/arrayproblem/DuplicateElement.java
@@ -1,6 +1,6 @@
 package com.arrayproblem;
 
-public class DublicateElement {
+public class DuplicateElement {
     public static void main(String args[]) {
         int arr[] = {5, 4, 3, 2, 1, 4, 3};
         System.out.println("Duplicate elements in array ");
