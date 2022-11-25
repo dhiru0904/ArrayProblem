4f17d3ad87e7dae3bbb1da0068462c4c27bc5a5d
dhiru0904 <dhirajdevre4@gmail.com>
2022-11-25 23:01:01 +0530

    Added: demonstrate to second number largest print

---
+++ src/com/arrayproblem/SecondLargest.java
@@ -0,0 +1,18 @@
+package com.arrayproblem;
+
+public class SecondLargest {
+    public static void main(String args[]) {
+        int arr[] = {11, 22, 33, 44,55, 99};
+        int temp, size;
+        size = arr.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 2; j > size; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                    arr[i] = arr[j];
+                   arr[j] = temp;
+                }
+
+        System.out.println("Second largest no is " + arr[size - 2]);
+    }
+}
